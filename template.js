6ccbb19d88f9663766752f1cc2fa581dd19697e8
吴浩敏 <wuhm@yeastar.com>
2020-03-14 16:47:31 +0800

    222

---
+++ template.js
@@ -0,0 +1,27 @@
+import { useState ,useEffect} from 'react';
+import PropTypes from 'prop-types';
+import './{{template}}.css';
+
+const {{ template }} = props => {
+  //  参数   方法
+  const [count, setCount] = useState(0);   
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+  return <div className="{{template}}">
+    {{ template }}
+    <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+  </div>
+}
+
+{{template}}.defaultProps = {
+};
+
+{{template}}.propTypes = {
+};
+
+export default {{ template }}
