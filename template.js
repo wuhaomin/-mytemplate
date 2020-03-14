e6eb1b24fd826eddb76b47307a03e77c5750cc81
吴浩敏 <wuhm@yeastar.com>
2020-03-14 16:53:51 +0800

    333

--- template.js
+++ template.js
@@ -1,27 +1,56 @@
-import { useState ,useEffect} from 'react';
-import PropTypes from 'prop-types';
+import * as React from 'react';
+import { withRouter } from 'react-router-dom'
 import './{{template}}.css';
+interface Props {
 
-const {{ template }} = props => {
-  //  参数   方法
-  const [count, setCount] = useState(0);   
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `You clicked ${count} times`;
-  });
-  return <div className="{{template}}">
-    {{ template }}
-    <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-  </div>
 }
+interface State {
 
-{{template}}.defaultProps = {
-};
+}
+@withRouter
 
-{{template}}.propTypes = {
-};
+  class {{ template }} extends React.Component < Props, State > {
+    constructor(props: Props) {
+      super(props);
+      this.state = {
 
-export default {{ template }}
+      };
+    }
+  
+    static getDerivedStateFromProps(nextProps: object, prevState: object) {
+
+      return null
+    }
+  
+  
+    componentDidMount() { }
+  
+  
+    shouldComponentUpdate(nextProps, nextState) {
+      return true;
+    }
+    // 记忆上一次
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+      // 如果 `props.list` 增加，将原来的 scrollHeight 存入 listRef
+      if (prevProps.list.length < this.props.list.length) {
+        return this.listRef.scrollHeight
+      }
+      return null
+    }
+  // snapshot 快照
+  
+    componentDidUpdate(prevProps, prevState, snapshot) { }
+  
+    componentWillUnmount() { }
+  
+  
+    render() {
+      return (
+        <div className="{{template}}">
+          {{ template }}
+        </div>
+      );
+    }
+  }
+
+export default {{template}}
